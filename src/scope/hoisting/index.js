dc1d75e67ec1c5c26c71593dab9e9b5240099014
Vicente Matias <vicente.matias.g@gmail.com>
2023-01-10 17:51:57 -0300

    Fin

---
+++ src/scope/hoisting/index.js
@@ -0,0 +1,13 @@
+//console.log(nameOfDog);
+
+//var nameOfDog = 'Elmo';
+
+//console.log(nameOfDog);
+
+
+var elmo = 'Elmito';
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
